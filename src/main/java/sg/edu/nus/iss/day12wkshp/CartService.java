fc7d366c18f01070c6b5ad6ae9fbb8ee193b559d
darryl1975 <darryl1975@hotmail.com>
2022-07-19 11:50:25 +0800

    CartService added

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartService.java
@@ -0,0 +1,34 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CartService {
+    
+    public List<Item> getShoppingItems() {
+
+        List<Item> lstItems = new ArrayList<>();
+
+        Item itm = new Item();
+        itm.setItemName("Prada Bag");
+        itm.setQuantity(5);
+        lstItems.add(itm);
+
+        itm = new Item();
+        itm.setItemName("Chanel Wallet");
+        itm.setQuantity(2);
+        lstItems.add(itm);
+
+        itm = new Item();
+        itm.setItemName("MacBook Pro");
+        itm.setQuantity(10);
+        lstItems.add(itm);
+
+        itm = new Item();
+        itm.setItemName("LV Tot Bag");
+        itm.setQuantity(8);
+        lstItems.add(itm);
+
+        return lstItems;
+    }
+}
